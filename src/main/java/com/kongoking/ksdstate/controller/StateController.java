379b2c9e4346382431f699058129b75e8ea103df
luonianxin <1423364324@qq.com>
2021-05-16 18:20:27 +0800

    1.统计详情页面增加了使用post请求获取数据的方式,(注意参数的传递方式,请求参数可以直接跟在url地址后,也可使用params参数对象的方式传递,后台接口需要增加@RequestBody注解,并使用DTO对象来接收前端传递的参数,不然无法正确接收参数); 2.解决直接使用Mustache标签，当出现较大网络延迟时会暴露Mustache标签的问题,解决方法：１．通过在整体实例的盒子上添加v-cloak指令并配合对应的css样式即可解决 ２．推荐使用v-html等指令来绑定数据

--- src/main/java/com/kongoking/ksdstate/controller/StateController.java
+++ src/main/java/com/kongoking/ksdstate/controller/StateController.java
@@ -1,6 +1,7 @@
 package com.kongoking.ksdstate.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kongoking.ksdstate.dto.StateDetailDTO;
 import com.kongoking.ksdstate.entity.State;
 import com.kongoking.ksdstate.entity.StateDetail;
 import com.kongoking.ksdstate.service.StateDetailService;
@@ -70,6 +71,37 @@ public class StateController {
         // 2.根据stateid查询统计对象明细
         QueryWrapper<StateDetail> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("stateid",stateId);
+        List<StateDetail> listDetail = stateDetailService.list(queryWrapper);
+        // 3.组装ｖｏ对象
+        StateVo stateVo = new StateVo();
+        BeanUtils.copyProperties(state,stateVo);
+        stateVo.setStateDetailList(listDetail);
+        // 4.将统一结果返回
+        return ResponseWrapper.ok().data("stateVo",stateVo);
+    }
+
+    /**
+     * @Description:　根据ｉｄ查询统计明细
+     * @Date: 2021/5/9 下午9:52
+     * @Author: 　　luonianxin
+     * @Email 1423364324@qq.com
+     * @Param: stateId
+     * @return com.kongoking.ksdstate.vo.ResponseWrapper
+     */
+    @PostMapping("/state/detailByPost")
+    @ResponseBody
+    public ResponseWrapper getStateDetailsByPostRequest(@RequestBody StateDetailDTO stateDetailDTO){
+        if(StringUtils.isEmpty(stateDetailDTO.getStateId())){
+            return ResponseWrapper.error().code(503).message("未找到统计项");
+        }
+        // 1.根据ｉｄ查询统计对象
+        State state = stateService.getById(stateDetailDTO.getStateId());
+        if(state == null){
+            return ResponseWrapper.error().code(503).message("未找到统计项");
+        }
+        // 2.根据stateid查询统计对象明细
+        QueryWrapper<StateDetail> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("stateid",stateDetailDTO.getStateId());
         List<StateDetail> listDetail = stateDetailService.list(queryWrapper);
         // 3.组装ｖｏ对象
         StateVo stateVo = new StateVo();
---
+++ src/main/java/com/kongoking/ksdstate/dto/StateDetailDTO.java
@@ -0,0 +1,21 @@
+package com.kongoking.ksdstate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author luonianxin
+ * @Description 统计详情查询参数
+ * @Date 2021/5/16
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class StateDetailDTO {
+    /** 统计项Ｉd **/
+    private Integer stateId;
+}
+
